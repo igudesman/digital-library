ed1ae9fcb6492d8c58a9ddd7d3fc3d5db0625006
Daniil Igudesman <digudesm@gmail.com>
2020-08-29 21:04:34 +0300

    uploading has been added

--- core/core/urls.py
+++ core/core/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from upload import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('upload/', views.upload, name='upload'),
 ]
