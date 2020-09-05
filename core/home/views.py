a2b68f812bad85301b6c1f8cb95819522bbaf354
Daniil Igudesman <digudesm@gmail.com>
2020-09-05 22:52:15 +0300

    home page updated

--- core/home/views.py
+++ core/home/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from upload.models import UploadedFile
 
 def home(request):
-	return render(request, 'home/home.html')
+    context = parse_data()
+    return render(request, 'home/home.html', context)
+
+
+def parse_data():
+    context = {'Book': [],
+               'Lecture': [],
+               'Assignment': []}
+    content = UploadedFile.objects.all()
+    for material in content:
+        context[str(material.material_type)].append(material)
+    return context
 
