7c5e3afa7a01982e4455dff95a884568757c17a5
Daniil Igudesman <digudesm@gmail.com>
2020-09-05 18:50:36 +0300

    library-content has been added

--- core/upload/views.py
+++ core/upload/views.py
@@ -5,9 +5,11 @@ from .forms import UploadedFileForm
 from .models import UploadedFile
 
 def upload(request):
+    status = True
     context = {'title': '',
                'author': '',
                'material_type': ''}
+
     if request.method == 'POST':
         file_name = request.FILES['file']
         file_type = request.POST['material_type']
@@ -22,11 +24,13 @@ def upload(request):
                 context['title'] = request.POST['title']
                 context['author'] = request.POST['author']
                 context['material_type'] = request.POST['material_type']
+                status = False
         except ValueError:
-        	print('SHIT2!') # TODO
-        if form.is_valid():
+            messages.error(request, 'Something went wrong!')
+            status = False
+        if form.is_valid() and status:
             form.save()
-
+            messages.success(request, '{0} {1} has been successefully added!'.format(file_type, file_name))
     else:
         form = UploadedFileForm()
     return render(request, 'upload/upload.html', context)
