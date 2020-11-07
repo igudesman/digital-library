3d747ab7f6999c73c9ffd85de8fd32d2957d3d4b
Orina1109 <a.kilmukhametova@innopolis.university>
2020-11-07 17:06:47 +0300

    implemented contexts for posts and files and coresponding api functions

--- context.py
+++ context.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+from bson import ObjectId
 from pymongo import MongoClient
 import typing
 
@@ -67,6 +70,159 @@ class UserDBModel:
         self.__role = obj_dict["role"]
         self.__id = str(obj_dict["_id"])
 
+
+class FileDBModel:
+    def __init__(self):
+        self.__id: str = ""
+        self.__name: str = ""
+        self.__type: str = ""
+        self.__path: str = ""
+        self.__size: int = 0
+
+    def from_dict(self, data: dict):
+        self.__id = str(data["_id"])
+        self.__name = data["name"]
+        self.__path = data["path"]
+        self.__type = data["type"]
+        self.__size = data["size"]
+
+    def to_response_dict(self) -> dict:
+        data = dict()
+        data["_id"] = self.__id
+        data["name"] = self.__name
+        data["type"] = self.__type
+        data["size"] = self.__size
+        return data
+
+    def to_db_dict(self) -> dict:
+        data = self.to_response_dict()
+        data["path"] = self.__path
+        return data
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value: int):
+        self.__size = value
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value: str):
+        self.__name = value
+
+    @property
+    def type(self):
+        return self.__type
+
+    @type.setter
+    def type(self, value: str):
+        self.__type = value
+
+    @property
+    def path(self):
+        return self.__path
+
+    @path.setter
+    def path(self, value: str):
+        self.__path = value
+
+
+class PostDBModel:
+
+    def __init__(self):
+        self.__id: str = ""
+        self.__title: str = ""
+        self.__description: str = ""
+        self.__category: str = ""
+        self.__tags: typing.List[str] = list()
+        self.__attachments: typing.List[str] = list()
+        self.__date: datetime = datetime.min
+        self.__publisher: str = ""
+
+    def from_dict(self, data: dict):
+        self.__id = str(data["_id"])
+        self.__title = data["title"]
+        self.__description = data["description"]
+        self.__category = data["category"]
+        self.__tags = data["tags"]
+        self.__attachments = data["attachments"]
+        self.__date = data["date"]
+        self.__publisher = data["publisher"]
+
+    def to_dict(self) -> dict:
+        data = dict()
+        data["_id"] = self.__id
+        data["title"] = self.__title
+        data["description"] = self.__description
+        data["category"] = self.__category
+        data["date"] = self.__date
+        data["publisher"] = self.__publisher
+        data["tags"] = self.__tags
+        data["attachments"] = self.__attachments
+        return data
+
+    @property
+    def publisher(self):
+        return self.__publisher
+
+    @publisher.setter
+    def publisher(self, value: str):
+        self.__publisher = value
+
+    @property
+    def date(self):
+        return self.__date
+
+    @date.setter
+    def date(self, value: datetime):
+        self.__date = value
+
+    @property
+    def attachments(self):
+        return self.__attachments
+
+    @attachments.setter
+    def attachments(self, value: typing.List[str]):
+        self.__attachments = value
+
+    @property
+    def tags(self):
+        return self.__tags
+
+    @tags.setter
+    def tags(self, value: typing.List[str]):
+        self.__tags = value
+
+    @property
+    def category(self):
+        return self.__category
+
+    @category.setter
+    def category(self, value: str):
+        self.__category = value
+
+    @property
+    def description(self):
+        return self.__description
+
+    @description.setter
+    def description(self, value: str):
+        self.__description = value
+
+    @property
+    def title(self):
+        return self.__title
+
+    @title.setter
+    def title(self, value: str):
+        self.__title = value
+
+
 class MainContext:
 
     def __init__(self):
@@ -76,6 +232,7 @@ class MainContext:
     @property
     def database(self):
         return self.__db
+
 
 class UserContext:
 
@@ -93,10 +250,11 @@ class UserContext:
         return user_model
 
     def change_password(self, user_name: str, password_hash_new: str):
-        self.__users_collection.update_one({"user_name": user_name}, {"password_hash": password_hash_new})
+        self.__users_collection.update_one({"user_name": user_name}, {"$set":{"password_hash": password_hash_new}})
 
     def delete_user(self, user_name: str):
         self.__users_collection.delete_one({"user_name": user_name})
+
 
 class FileContext:
 
@@ -104,3 +262,84 @@ class FileContext:
         self.__context = main_context
         self.__files_collection = self.__context.database.get_collection("files")
 
+    def add_file(self, file_model: FileDBModel):
+        data = file_model.to_db_dict()
+        file_id = self.__files_collection.insert_one(data)
+        return str(file_id.inserted_id)
+
+    def find_file(self, file_id: str):
+        found = self.__files_collection.find({"_id": ObjectId(file_id)})
+        if found.count() != 1:
+            return None
+        file_data = found[0]
+        file_model = FileDBModel()
+        file_model.from_dict(file_data)
+        return file_model
+
+
+class ListContext:
+    def __init__(self, context: MainContext):
+        self.__context = context
+        self.__wait_list_collection = self.__context.database.get_collection("wait_list")
+        self.__main_list_collection = self.__context.database.get_collection("main_list")
+        self.__search_str = ""
+        self.__category = ""
+        self.__tags = None
+
+    def accept_post(self, post_id: str):
+        post = self.find_post(post_id, "wait")
+        self.delete_post(post_id, "wait")
+        self.add_post(post, "main")
+
+    def find_post(self, post_id: str, collection: str = "main"):
+        if collection == "main":
+            found_posts = self.__main_list_collection.find({"_id": ObjectId(post_id)})
+        else:
+            found_posts = self.__wait_list_collection.find({"_id": ObjectId(post_id)})
+        if found_posts.count() == 0:
+            return None
+        post = PostDBModel()
+        post.from_dict(found_posts[0])
+        return post
+
+    def delete_post(self, post_id: str, collection: str = "main"):
+        if collection == "main":
+            self.__main_list_collection.delete_one({"_id": ObjectId(post_id)})
+        else:
+            self.__wait_list_collection.delete_one({"_id": ObjectId(post_id)})
+
+    def add_post(self, post_data: PostDBModel, collection: str = "wait"):
+        if collection == "main":
+            self.__main_list_collection.insert_one(post_data.to_dict())
+        else:
+            self.__wait_list_collection.insert_one(post_data.to_dict())
+
+    def __filter(self, elem: dict) -> bool:
+        title: str = elem["title"].lower()
+        category: str = elem["category"].lower()
+        tags: typing.List[str] = elem["tags"]
+        if self.__search_str != "" and title.find(self.__search_str) == -1:
+            return False
+        if self.__category is not None and category != self.__category:
+            return False
+        if self.__tags is not None:
+            for tag in self.__tags:
+                if tag not in tags:
+                    return False
+
+        return True
+
+    def search(self, collection: str = "main", search_string: str = "", category: typing.Optional[str] = None,
+               tags: typing.List[str] = None, offset: int = 0, limit: int = 20):
+        self.__search_str = search_string
+        self.__category = category
+        self.__tags = tags
+        if collection == "wait":
+            found = list(self.__wait_list_collection.find()[offset:offset + limit])
+        else:
+            found = list(self.__main_list_collection.find()[offset:offset + limit])
+        filtered = filter(self.__filter, found)
+        for post in filtered:
+            model = PostDBModel()
+            model.from_dict(post)
+            yield model
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from flask import Flask, request
-from pymongo import MongoClient, collection
+from flask import Flask, request, jsonify, send_file
 import context
 import uuid
 import datetime
@@ -8,9 +7,11 @@ import jwt
 app = Flask(__name__)
 main_context = context.MainContext()
 user_context = context.UserContext(main_context)
+list_context = context.ListContext(main_context)
+files = context.FileContext(main_context)
 
 
-@app.route('/api/login')
+@app.route('/api/user/login')
 def login():
     username = request.args.get("user_name")
     passhash = request.args.get("password_hash")
@@ -18,9 +19,9 @@ def login():
     user = user_context.find_user(username)
 
     if user is None:
-        return 'User not found'
+        return 'User not found', 404
     if passhash != user.password_hash:
-        return 'Incorrect password'
+        return 'Incorrect password', 401
 
     # delau token
     payload = dict()
@@ -33,8 +34,46 @@ def login():
     return encoded_jwt
 
 
+@app.route('/api/user/change_password')
+def change_password():
+    token = request.args.get("token")
+    old_hash = request.args.get("old_hash")
+    new_hash = request.args.get("new_hash")
+    if new_hash is None or new_hash.strip() == "":
+        return "Invalid new password", 403
+    if old_hash is None or old_hash.strip() == "":
+        return "Invalid old password", 403
+
+    payload = jwt.decode(token, key="Pisos6Blyat9Chlen", algorithms=["HS256"])
+    user = user_context.find_user(payload["user_name"])
+    if user is None:
+        return "User not found", 404
+    if old_hash != user.password_hash:
+        return "Old and new password are different", 403
+
+    user_context.change_password(payload["user_name"], new_hash)
+    return ""
+
+
+@app.route("/api/user/delete")
+def delete_user():
+    token = request.args.get("token")
+    user_name = request.args.get("user_name")
+    payload = jwt.decode(token, key="Pisos6Blyat9Chlen", algorithms=["HS256"])
+    if payload["role"] != "admin":
+        return "Access denied", 403
+
+    user = user_context.find_user(user_name)
+    if user is None:
+        return "User not found", 404
+    if user.role == "admin":
+        return "Access denied", 403
+    user_context.delete_user(user_name)
+    return ""
+
+
 # zagruzka faila
-@app.route('/api/upload_file', methods=["POST"])
+@app.route('/api/files/upload', methods=["POST"])
 def upload_file():
     file_name = request.args.get("file_name")
     token = request.args.get("token")
@@ -45,54 +84,144 @@ def upload_file():
     file_stream.write(stream.read())
     file_stream.flush()
     file_stream.close()
-    file_note = dict()
-    file_note["name"] = file_name
-    file_note["type"] = file_name[file_name.index(".") + 1:]
-    file_note["size"] = stream.limit
-    file_note["path"] = "files/" + file_name_uuid
-    file_id = str(files.insert_one(file_note).inserted_id)
-    return file_id
+    file_type = file_name[file_name.index(".") + 1:]
+    file = context.FileDBModel()
+    file.name = file_name
+    file.type = file_type
+    file.size = stream.limit
+    file.path = 'files/' + file_name_uuid
+    return files.add_file(file)
+
+
+@app.route('/api/files/get_info')
+def get_file_info():
+    token = request.args.get("token")
+    file_id = request.args.get("file_id")
+    payload = jwt.decode(token, key="Pisos6Blyat9Chlen", algorithms=["HS256"])
+    file = files.find_file(file_id)
+    if file is None:
+        return "File not found", 404
+    return file.to_response_dict()
+
+
+@app.route('/api/files/download')
+def download_file():
+    token = request.args.get("token")
+    file_id = request.args.get("file_id")
+    payload = jwt.decode(token, key="Pisos6Blyat9Chlen", algorithms=["HS256"])
+    file = files.find_file(file_id)
+    if file is None:
+        return "File not found", 404
+    return send_file(file.path, as_attachment=True, attachment_filename=file.name)
 
 
 # publikyu post
-@app.route('/api/upload_post', methods=["POST"])
+@app.route('/api/post/upload', methods=["POST"])
 def upload_post():
     token = request.args.get("token")
     payload = jwt.decode(token, key="Pisos6Blyat9Chlen", algorithms=["HS256"])
     post_data = request.json
-    post_node = dict()
-    post_node["title"] = post_data["title"]
-    post_node["description"] = post_data["description"]
-    post_node["attachments"] = post_data["attachments"]
-    post_node["category"] = post_data["category"]
-    post_node["tags"] = post_data["tags"]
-    post_node["publisher"] = payload["user_id"]
-    post_node["date"] = datetime.datetime.now()
+    post = context.PostDBModel()
+    post.title = post_data["title"]
+    post.description = post_data["description"]
+    post.attachments = post_data["attachments"]
+    post.category = post_data["category"]
+    post.tags = post_data["tags"]
+    post.publisher = payload["user_id"]
+    post.date = datetime.datetime.now()
     if payload["role"] == "student":
-        wait_list.insert_one(post_node)
+        list_context.add_post(post, "wait")
     else:
-        main_list.insert_one(post_node)
+        list_context.add_post(post, "main")
     return ""
 
 
 # uydalenie posta
-@app.route("/api/delete_post")
+@app.route("/api/post/delete")
 def delete_post():
     token = request.args.get("token")
     post_id = request.args.get("post_id")
+    collection = request.args.get("collection")
+    if collection is None or collection.strip() == "":
+        collection = "main"
     payload = jwt.decode(token, key="Pisos6Blyat9Chlen", algorithms=["HS256"])
-    found_posts = main_list.find({"_id": collection.ObjectId(post_id)})
-    if found_posts.count() == 0:
-        return "Post not found"
-    post = found_posts[0]
+
+    post = list_context.find_post(post_id, collection)
+    if post is None:
+        return "Post not found", 404
+
     if payload["role"] == "admin":
-        main_list.delete_one({"_id": collection.ObjectId(post_id)})
+        list_context.delete_post(post_id, collection)
         return ""
 
     if payload["user_id"] == post["publisher"]:
-        main_list.delete_one({"_id": collection.ObjectId(post_id)})
+        list_context.delete_post(post_id, collection)
         return ""
-    return "Access denied"
+    return "Access denied", 403
+
+
+@app.route("/api/post/accept")
+def accept_post():
+    token = request.args.get("token")
+    post_id = request.args.get("post_id")
+    payload = jwt.decode(token, key="Pisos6Blyat9Chlen", algorithms=["HS256"])
+    if payload["role"] == "student":
+        return "Access denied", 403
+    else:
+        post = list_context.find_post(post_id, "wait")
+        if post is None:
+            return "Post not found", 404
+        list_context.accept_post(post_id)
+        return ""
+
+
+#poisk
+@app.route("/api/post/search")
+def search():
+    token = request.args.get("token")
+    search_str = request.args.get("search_str")
+    if search_str is None:
+        search_str = ""
+    else:
+        search_str = search_str.strip().lower()
+
+    category = request.args.get("category")
+    if category is not None:
+        category = category.strip().lower()
+    tags = request.args.get("tags")
+    if tags is not None and tags.strip().lower() != "":
+        tags = tags.strip().lower().split(",")
+    offset = request.args.get("offset")
+    limit = request.args.get("limit")
+    if offset is None:
+        offset = 0
+    else:
+        try:
+            offset = int(offset)
+        except ValueError:
+            offset = 0
+    if limit is None:
+        limit = 20
+    else:
+        try:
+            limit = int(limit)
+        except ValueError:
+            limit = 20
+    collection = request.args.get("collection")
+    if collection is None or collection.strip() == "":
+        collection = "main"
+
+    payload = jwt.decode(token, key="Pisos6Blyat9Chlen", algorithms=["HS256"])
+    if collection == "wait" and payload["role"] != "student":
+        result = list_context.search("wait", search_str, category, tags, offset, limit)
+    elif collection == "main":
+        result = list_context.search("main", search_str, category, tags, offset, limit)
+    else:
+        return "Access denied", 403
+    response = list()
+    for node in result:
+        response.append(node.to_dict())
+    return jsonify(response)
 
 
 if __name__ == '__main__':
